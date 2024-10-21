2233cdb8a081bc9a1f7651ee3063b9a7c8aa1f05
Dasaav-dsv <simplydasaav@gmail.com>
2024-10-21 05:03:40 +0200

    New patches, use better pattern scanning

---
+++ source/dllmain.cpp
@@ -0,0 +1,71 @@
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <thread>
+#include <span>
+
+#include "index.hpp"
+#include "suspend.hpp"
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#include <psapi.h>
+
+constexpr Index::CPattern check1s = "7A ?? 75 ?? B9 ?? ?? ?? ?? E8 ?? ?? ?? ?? F3 0F 11 05";
+constexpr Index::CPattern check1l = "0F 8A ?? ?? ?? ?? 0F 85 ?? ?? ?? ?? B9 ?? ?? ?? ?? E8 ?? ?? ?? ?? F3 0F 11 05";
+constexpr Index::CPattern check2 =  "73 ?? 0F 2F ?? 76 ?? 48 8D 15";
+constexpr Index::CPattern check3 =  "72 ?? 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 90 48 8B 05 ?? ?? ?? ?? FF D0";
+
+std::span<uint8_t> getExeImage() noexcept {
+    MODULEINFO mInfo;
+    HANDLE pHandle = GetCurrentProcess();
+    HMODULE pModule = GetModuleHandle(NULL);
+    if (!GetModuleInformation(pHandle, pModule, &mInfo, sizeof(mInfo)))
+        return {};
+    return std::span(reinterpret_cast<uint8_t*>(mInfo.lpBaseOfDll),
+        mInfo.SizeOfImage);
+}
+
+template <typename T>
+inline void writeProtected(void* p, T value) noexcept {
+    DWORD _;
+    VirtualProtect(p, sizeof(value), PAGE_EXECUTE_READWRITE, &_);
+    std::memcpy(p, &value, sizeof(value));
+}
+
+void patchDbgChecks() try {
+    SuspendMutex suspendThreads;
+    std::scoped_lock lock{ suspendThreads };
+    auto exe = getExeImage();
+    auto index = Index::Bytes(exe);
+    // Timed check 1, short pattern:
+    for (auto i : index.findAll(check1s)) {
+        writeProtected<uint8_t>(&exe[i + 1], 0x02);
+        writeProtected<uint8_t>(&exe[i + 3], 0x00);
+    }
+    // Timed check 1, long pattern:
+    for (auto i : index.findAll(check1l)) {
+        writeProtected<int32_t>(&exe[i + 2], 0x0000'0006);
+        writeProtected<int32_t>(&exe[i + 8], 0x0000'0000);
+    }
+    // Timed check 2:
+    for (auto i : index.findAll(check2))
+        writeProtected<uint8_t>(&exe[i + 1], 0x00);
+    // Timed check 3 (can't edit displacement, this ends up
+    // modifying about 10 bytes of actual bytecode):
+    for (auto i : index.findAll(check3))
+        writeProtected<uint8_t>(&exe[i], 0xEB);
+    FlushInstructionCache(GetCurrentProcess(), NULL, 0);
+}
+catch (const std::exception& e) {
+    OutputDebugStringA(e.what());
+}
+
+int DllMain(HMODULE, DWORD reason, LPVOID) {
+    if (reason == DLL_PROCESS_ATTACH) {
+        std::thread thread{ &patchDbgChecks };
+        thread.detach();
+    }
+    return TRUE;
+}
